0ad48d3882f2171acd23e4f23ea18aa9f2a82849
DavidXT <David.xt.nguyen@gmail.com>
2023-05-25 11:28:34 +0200

    SFX

--- Assets/Scripts/Object/Magnet.cs
+++ Assets/Scripts/Object/Magnet.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Magnet : MonoBehaviour
-{
-   private void OnTriggerEnter(Collider other)
+{    
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
